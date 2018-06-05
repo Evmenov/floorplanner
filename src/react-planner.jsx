ab42ef8dfe481afd57f47c89aed2d52ae2fa0437
Alexandr <sanyaxxxl@mail.ru>
2018-06-05 14:02:18 +0300

    added new method

--- src/react-planner.jsx
+++ src/react-planner.jsx
@@ -41,6 +41,19 @@ class ReactPlanner extends Component {
     };
   }
 
+  componentDidMount () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
 
   componentDidMount(){
     fetch("localhost")
